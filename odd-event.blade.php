0c596606d9fcca97629944fd784b091fbf59ea3f
prince <prince.suhagiya@shunyavkash.com>
2022-12-17 15:12:37 +0530

    js programs parctise

---
+++ odd-event.blade.php
@@ -0,0 +1,20 @@
+
+<!-- Odd Or even -->
+
+<script text="text/javascript">
+
+
+let num = (parseInt(prompt("Enter Number :")));
+
+
+if (num % 2 == 0) {
+
+document.write("This value is Even :" + num);
+} else {
+
+document.write("This value is Odd :" + num);
+}
+
+</script>
+
+
